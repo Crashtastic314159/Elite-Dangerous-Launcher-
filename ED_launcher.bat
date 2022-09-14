207d3e9e1280acc2272ed6c15dd79bd8d1a995ec
Crashtastic314159 <97134474+Crashtastic314159@users.noreply.github.com>
2022-09-14 18:26:15 +1000

    Update ED_launcher.bat

--- ED_launcher.bat
+++ ED_launcher.bat
@@ -1,12 +1,18 @@
 @echo off
 
+:: The installation path for EDMC 
 cd "C:\Program Files (x86)\EDMarketConnector" 
 start  EDMarketConnector.exe /bc
+
+:: The installation path for EDx52Display. To disable, delete/comment the next two lines
 cd /d "D:\Games\EDLaunch\x52" 
 start  EDx52Display.exe /bc
+
+:: The installation path for the game
 cd /d "D:\Games\EDLaunch" 
 start /w EDLaunch.exe /bc
 
+::Main loop
 :LOOP
 tasklist | find /i "EDLaunch" >nul 2>&1
 IF ERRORLEVEL 1 (
